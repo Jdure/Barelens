28c14517938ea24b99b7c93d5335bbea532a0eee
Jdure <22172148+Jdure@users.noreply.github.com>
2022-12-05 15:43:20 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+import React, { Key } from "react"
+import { GetStaticProps } from "next"
+import Image from "next/image";
+
+
+interface Images {
+  id: string;
+  title: string;
+  image: string;
+  width: Number | undefined;
+  height: Number | undefined;
+  created_at: Date;
+}
+
+export default function App<T>( props: { images: Images[]; }) {
+  return (
+    <div>
+        {props.images.map((item: Images, idx: Key) => {
+          return (
+            <>
+              <p key={idx}>{item.title}</p>
+              <Image width={500} height={500} alt={item.title} src={item.image} />
+            </>
+          );
+        })}
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch(
+    `https://${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/search`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        expression: `folder=test`
+      }),
+    }
+  )
+  const {resources} = await res.json()
+
+  const images : Images[] = resources.map((value: { public_id: string; filename: string; secure_url: string; width: Number, height: Number, created_at: Date }) => {
+    const { created_at, width, height } = value;
+    return {
+        id: value.public_id,
+        title: value.filename,
+        image: value.secure_url,
+        created_at,
+        width,
+        height
+    }
+  })
+
+  return {
+    props: {
+      images,
+    },
+  }
+}
