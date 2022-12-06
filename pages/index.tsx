8aecfcfc331925449936d10c6ecdac5594e33bd8
Jdure <22172148+Jdure@users.noreply.github.com>
2022-12-06 09:00:45 -0500

    added linting + prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import React, { Key } from "react"
-import { GetStaticProps } from "next"
+import React, { Key } from "react";
+import { GetStaticProps } from "next";
 import Image from "next/image";
-
 
 interface Images {
   id: string;
@@ -12,17 +11,17 @@ interface Images {
   created_at: Date;
 }
 
-export default function App<T>( props: { images: Images[]; }) {
+export default function App<T>(props: { images: Images[] }) {
   return (
     <div>
-        {props.images.map((item: Images, idx: Key) => {
-          return (
-            <>
-              <p key={idx}>{item.title}</p>
-              <Image width={500} height={500} alt={item.title} src={item.image} />
-            </>
-          );
-        })}
+      {props.images.map((item: Images, idx: Key) => {
+        return (
+          <>
+            <p key={idx}>{item.title}</p>
+            <Image width={500} height={500} alt={item.title} src={item.image} />
+          </>
+        );
+      })}
     </div>
   );
 }
@@ -36,27 +35,36 @@ export const getStaticProps: GetStaticProps = async (context) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        expression: `folder=test`
+        expression: `folder=test`,
       }),
     }
-  )
-  const {resources} = await res.json()
+  );
+  const { resources } = await res.json();
 
-  const images : Images[] = resources.map((value: { public_id: string; filename: string; secure_url: string; width: Number, height: Number, created_at: Date }) => {
-    const { created_at, width, height } = value;
-    return {
+  const images: Images[] = resources.map(
+    (value: {
+      public_id: string;
+      filename: string;
+      secure_url: string;
+      width: Number;
+      height: Number;
+      created_at: Date;
+    }) => {
+      const { created_at, width, height } = value;
+      return {
         id: value.public_id,
         title: value.filename,
         image: value.secure_url,
         created_at,
         width,
-        height
+        height,
+      };
     }
-  })
+  );
 
   return {
     props: {
       images,
     },
-  }
-}
+  };
+};
