8a2b38d088bf47c2ad98839ecb48cd374698de11
Jdure <22172148+Jdure@users.noreply.github.com>
2023-01-30 17:51:20 -0500

    added revalidation file

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+// TODO: Revalidate dynamically - Should be able to revalidate [home, service and about]
+// TODO: deploy site to host to test revalidation properly
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+        return res.status(401).json({ message: "Invalid token" })
+    }
+
+    try {
+        console.log(req.query)
+        await res.revalidate("/")
+        return res.json({ revalidated: true })
+    } catch (err) {
+        return res.status(500).send("Error revalidating")
+    }
+}
