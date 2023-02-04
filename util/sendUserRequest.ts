95969dabd926375547d51727005f9a2f334365ef
Jdure <22172148+Jdure@users.noreply.github.com>
2023-02-03 21:07:14 -0500

    removed cors

--- util/sendUserRequest.ts
+++ util/sendUserRequest.ts
@@ -3,7 +3,6 @@ import React from "react"
 export default async function sendRequest(reqObj: Object) {
     await fetch("api/request", {
         method: "POST",
-        mode: "same-origin",
         headers: {
             "Content-Type": "application/json",
             Authentication: `Bearer ${process.env.DIRECTUS_DOCKER_TOKEN}`,
