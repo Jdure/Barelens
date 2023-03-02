eb304009697bc61bccae1b7473a503622609e971
Jdure <22172148+Jdure@users.noreply.github.com>
2023-03-01 22:56:05 -0500

    added: animation

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -17,7 +17,7 @@ export function ImageCard({
     plan,
 }: ImageCardProps) {
     return (
-        <div className="relative h-auto w-full p-3 rounded-sm drop-shadow-md md:w-1/3 hover:drop-shadow-xl">
+        <div className="relative h-auto w-full p-3 rounded-sm md:w-1/3">
             <Image
                 src={imgPath || ""}
                 alt={imgTitle}
@@ -25,12 +25,14 @@ export function ImageCard({
                 width={720}
                 height={1280}
             />
-            <div className="absolute inset-0 m-3 rounded-sm flex flex-col items-center justify-around text-transparent ease-in duration-300 sm:m-0 sm:inset-4 hover:text-white hover:backdrop-brightness-50">
-                <h3 className="font-headings capitalize">{plan}</h3>
-                <p className="text-base text-center font-extrabold sm:text-xl">
+            <div className="group absolute bottom-0 inset-x-0 inset-y-3/4 transform duration-700 ease-in-out hover:inset-y-0 hover:-translate-y-0.5 m-3 rounded-sm flex flex-col items-center justify-around text-white font-bold bg-gradient-to-t from-black">
+                <h3 className="font-headings capitalize text-3xl">{plan}</h3>
+                <p className=" leading-relaxed text-base invisible group-hover:visible transform group-hover:delay-300 text-center sm:text-xl">
                     {desc}
                 </p>
-                <p className="text-2xl font-extrabold">{price}</p>
+                <p className="text-2xl invisible group-hover:visible transform group-hover:delay-300">
+                    {price}
+                </p>
             </div>
         </div>
     )
